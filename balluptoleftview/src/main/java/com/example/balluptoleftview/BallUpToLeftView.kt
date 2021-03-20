c332a88bec0766b4a556adfa88c64fa1cf0d5af9
Anwesh43 <anweshthecool0@gmail.com>
2021-03-21 02:05:23 +0530

    adding constants and libraries

--- balluptoleftview/src/main/java/com/example/balluptoleftview/BallUpToLeftView.kt
+++ balluptoleftview/src/main/java/com/example/balluptoleftview/BallUpToLeftView.kt
@@ -1,2 +1,26 @@
 package com.example.balluptoleftview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#006064",
+    "#1A237E",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 10.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
