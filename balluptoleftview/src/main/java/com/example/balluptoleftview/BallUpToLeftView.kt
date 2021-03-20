ca07ca189be64f9520646036479f1ef65483e023
Anwesh43 <anweshthecool0@gmail.com>
2021-03-21 02:22:19 +0530

    adding BallUpToLeftView class

--- balluptoleftview/src/main/java/com/example/balluptoleftview/BallUpToLeftView.kt
+++ balluptoleftview/src/main/java/com/example/balluptoleftview/BallUpToLeftView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawBUTLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallUpToLeft(scale, w, h, paint)
 }
+
+class BallUpToLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
